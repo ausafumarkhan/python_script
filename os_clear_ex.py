20352703b9b812e3b5da52b79fdfaf4440fc4681
ausafumarkhan <ausafumarkhan@gmail.com>
2022-05-20 01:58:14 +0530

    script will clear terminal depending on OS

---
+++ os_clear_ex.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 20 01:48:41 2022
+
+@author: ausafumarkhan
+"""
+import platform
+import os
+
+if platform.system()=="Windows":
+    os.system("cls")
+else:
+    os.system("clear")
+        
